36a6c454fe11a69fa710ce3c4a01493f746c85ab
jamailun <jamailun@laposte.net>
2020-03-22 11:09:26 +0100

    Created custom button
    But Android Studio have problems xd

--- Application/AS/app/src/main/java/fr/insa/moove/bdd/jo/JoEvent.java
+++ Application/AS/app/src/main/java/fr/insa/moove/bdd/jo/JoEvent.java
@@ -4,6 +4,12 @@ import java.util.Date;
 
 public interface JoEvent {
 
+    public String getDescription();
+
+    public String getName();
+
+    public String getSport();
+
     public JoLocation getLocation();
 
     public Date getDate();
