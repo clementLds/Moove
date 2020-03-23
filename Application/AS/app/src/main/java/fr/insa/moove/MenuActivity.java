3ae8cd8dcf1703dfdb887367be4bff17bfa58719
jamailun <jamailun@laposte.net>
2020-03-23 18:54:11 +0100

    Functionnal MenuPage v1
    Upgraded DataBase System.
    Created all buttons.

--- Application/AS/app/src/main/java/fr/insa/moove/MenuActivity.java
+++ Application/AS/app/src/main/java/fr/insa/moove/MenuActivity.java
@@ -5,8 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+
+import java.util.UUID;
+
+import fr.insa.moove.bdd.DataBase;
+import fr.insa.moove.bdd.TestDataBase;
+import fr.insa.moove.bdd.clients.ClientAccount;
+import fr.insa.moove.bdd.jo.JoEvent;
 
 public class MenuActivity extends AppCompatActivity {
+
+    /**
+     * Database data
+     */
+    private DataBase bdd;
+    private ClientAccount client;
+
+    /**
+     * Fields to modify
+     */
+    TextView textFutureL1, textFutureL2;
+
+    private final static UUID clientUUID = UUID.randomUUID();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +36,36 @@ public class MenuActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_menu);
+
+        connectDataBase();
+        getViewsFields();
+        updateFutureEvents();
     }
+
+    /**
+     * Connect to the database and get the #client field
+     */
+    private void connectDataBase() {
+        bdd = new TestDataBase(clientUUID);
+        bdd.connect();
+        client = bdd.getAccountWithUUID(clientUUID);
+    }
+
+    /**
+     * get views fields
+     */
+    private void getViewsFields() {
+        textFutureL1 = findViewById(R.id.futureEvent1);
+        textFutureL2 = findViewById(R.id.futureEvent2);
+    }
+
+    /**
+     * Update the "future event" TextViews
+     */
+    public void updateFutureEvents() {
+        JoEvent event = client.getFutureEvent();
+        textFutureL1.setText(event.getSport() + "-" + event.getDate().toString());
+        textFutureL2.setText(event.getDescription());
+    }
+
 }
--- Application/AS/app/src/main/java/fr/insa/moove/bdd/clients/ClientAccount.java
+++ Application/AS/app/src/main/java/fr/insa/moove/bdd/clients/ClientAccount.java
@@ -1,7 +1,10 @@
 package fr.insa.moove.bdd.clients;
 
 import java.util.Date;
+import java.util.Set;
 import java.util.UUID;
+
+import fr.insa.moove.bdd.jo.JoEvent;
 
 public interface ClientAccount {
 
@@ -14,5 +17,11 @@ public interface ClientAccount {
     public Date getBirthDay();
 
     public ClientBalance getBalance();
+
+    public Set<JoEvent> getAllEvents();
+
+    public JoEvent getFutureEvent();
+
+    public boolean registerPresence(JoEvent event);
 
 }
